983c63914fb034ca7f60f7c735dc48e8617f078a
Hessam <hessam.imen@gmail.com>
2024-02-06 17:51:52 +1100

    feat: Extract data on the Catch-All

--- pages/events/[...slug].jsx
+++ pages/events/[...slug].jsx
@@ -1,4 +1,40 @@
+import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../dummy-data";
+
 function FilteredEventsPage() {
+  const router = useRouter();
+
+  const filterDate = router.query.slug;
+
+  if (!filterDate) {
+    return <p className="center">Loading ...</p>;
+  }
+
+  const filteredYear = filterDate[0];
+  const filteredMonth = filterDate[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    // NaN(numYear) ||
+    // NaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid Filter Please Adjust Your Values</p>;
+  }
+
+  const dateFilter = { year: numYear, month: numMonth };
+
+  const filteredEvents = getFilteredEvents(dateFilter);
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No Events Found for The Chosen Dates</p>;
+  }
+
   return (
     <div>
       <h1>Filtered Events Page</h1>
