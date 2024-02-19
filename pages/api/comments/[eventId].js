d8d5a901d4919b5e3347e8c1c216962c2e913d59
Hessam <hessam.imen@gmail.com>
2024-02-19 12:17:00 +1100

    feat: Add Comments API route

---
+++ pages/api/comments/[eventId].js
@@ -0,0 +1,41 @@
+function handler(req, res) {
+  const eventId = req.query.eventId;
+
+  if (req.method === "POST") {
+    const email = req.body.email;
+    const name = req.body.name;
+    const text = req.body.text;
+
+    if (
+      !email.includes("@") ||
+      !name ||
+      name.trim() === " " ||
+      !text ||
+      text.trim() === " "
+    ) {
+      res.status(422).json({ message: "Invalid input.", comment: newComment });
+      return;
+    }
+
+    const newComment = {
+      id: new Date().toISOString(),
+      email: email,
+      name: name,
+      text: text,
+    };
+
+    console.log(newComment);
+    res.status(201).json({ message: "success" });
+  }
+  if (req.method === "GET") {
+    const dummyList = [
+      { id: "c1", name: "user1", text: "first comment" },
+      { id: "c2", name: "user2", text: "second comment" },
+      { id: "c3", name: "user3", text: "third comment" },
+    ];
+
+    res.status(200).json({ comments: dummyList });
+  }
+}
+
+export default handler;
